98df1a409b0502b400a21222f77705fe14e3543c
jhm9318 <jhm9318@gmail.com>
2013-03-12 16:40:08 +0330

    encryption function now is working

--- Hill.cpp
+++ Hill.cpp
@@ -1,35 +1,82 @@
 #include "Hill.h"
 #include <cctype>
 
-using namespace std;
+//using namespace std;
 
+/*************************Constructors*********************************/
 Hill::Hill()
 {
     size = 2;
-    key = MatrixXd::Random(size, size);
+    key = MatrixXd::Random(size, size);    
 }
 
-Hill::Hill(int size)
-{    
-    key = MatrixXd::Random(size, size);
+Hill::Hill(int sz)
+{   
+    size = sz; 
+    key = MatrixXd::Random(sz, sz);
 }
+/*************************Constructors*********************************/
 
-void Hill::setMatrix(int col, int row, int val)
+/*********************setters and getters******************************/
+void Hill::setKey(int col, int row, int val)
 {
-    //key(row, col) = val;
+    key(row, col) = val;
 }
 
-string Hill::prepare(string plain)
+void Hill::setSize(int sz)
 {
-    string str="";
-    for(unsigned int i=0;i<plain.length();i++)
-        if( isalpha(plain.at(i)) )
-            str += tolower( plain.at(i) );
-                                            
-    return str;
+    size = sz;
+    key = MatrixXd::Random(sz, sz);
 }
 
 MatrixXd Hill::getKey()
 {
     return key;
+}
+/*********************setters and getters******************************/
+
+std::string Hill::prepare(std::string plain)
+{
+    std::string str="";
+    for(unsigned int i=0;i<plain.length();i++)
+        if( isalpha(plain.at(i)) )
+            str += std::tolower( plain.at(i) );
+                                            
+    return str;
+}
+
+void Hill::encryption()
+{
+    std::ifstream inputFile;
+    std::ofstream outputFile;
+    std::string tmp;
+    std::string buffer="";
+    
+    inputFile.open("plaint.txt");
+    outputFile.open("cipher.txt");
+    
+    while( getline(inputFile, tmp) )
+    {
+        tmp = prepare(tmp);
+        for(unsigned int i=0;i<tmp.length();i+=size)
+        {
+            MatrixXd p= MatrixXd(size,1);
+            MatrixXd c;
+            for(int j=0;j<size;j++)
+                if( i+j < tmp.length() )
+                    p(j,0) = tmp.at(i+j)-'a';            
+            
+            c = key*p;            
+            buffer="";
+            for(int i=0;i<size;i++)
+                buffer += c(i,0);
+            outputFile << buffer;
+        }
+        outputFile << '\n';
+    }        
+}
+
+void Hill::decryption()
+{
+    
 }
