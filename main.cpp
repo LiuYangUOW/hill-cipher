2e5a3ae51e5e8c7395c1bdf405e7a71c3317ec75
jhm9318 <jhm9318@gmail.com>
2013-03-12 12:51:09 +0330

    update main file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
-#include "Eigen/Core"
-
-using namespace Eigen;
+#include "Hill.cpp"
 
 int main()
-{
-    Matrix3f m3;
+{    
+    Hill h;
+    
+    cout<<h.getKey()<<endl;
+    cout<<h.prepare("Mohahhad 12 as xc .a");
+    
+    
     return 0;
 }
