241f23aa0eda54ff3999424fd0c22b1f69803dab
jhm9318 <jhm9318@gmail.com>
2013-03-13 23:06:32 +0330

    update main file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "Hill.cpp"
+using namespace std;
 
 int main()
 {    
+        
     Hill h;
+        
+    h.setSize(2);    
     
-    cout<<h.getKey()<<endl;
-    cout<<h.prepare("Mohahhad 12 as xc .a");
+    h.setKey(0,0,3); h.setKey(0,1,4);
+    h.setKey(1,0,1); h.setKey(1,1,3);
     
-    
+    //h.encryption();
+    h.decryption();
+     
     return 0;
 }
