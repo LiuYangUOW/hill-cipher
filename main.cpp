97840f493f0614da64513ca4f974e24861cff042
jhm9318 <jhm9318@gmail.com>
2013-04-15 14:44:28 +0430

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,37 @@ int main()
 {    
         
     Hill h;
-        
-    h.setSize(2);    
+    int size;
     
-    h.setKey(0,0,3); h.setKey(0,1,4);
+    cout<<"Please enter size of matrix: ";
+    cin>>size;
+        
+    h.setSize(size);
+    
+    h.setKey(0,1,4);
     h.setKey(1,0,1); h.setKey(1,1,3);
     
-    //h.encryption();
-    h.decryption();
+    for(int i=0;i<size;i++)
+        for(int j=0;j<size;j++)
+        {
+            int value;
+            cout<<"enter key["<<i+1<<"]["<<j+1<<"]: ";
+            cin>>value;
+            h.setKey(j,i,value);
+        }
+    
+    cout<<endl<<endl;
+    cout<<"your key is:"<<endl;
+    cout<<h.getKey()<<endl<<endl;
+    
+    char choice;
+    cout<<"encryption or decryption (e/d)? ";
+    cin>>choice;
+    
+    if ( choice == 'e' )
+        h.encryption();
+    else if ( choice == 'd' )
+        h.decryption();
      
     return 0;
 }
